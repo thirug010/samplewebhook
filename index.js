b5d9541e16a3f11c575bd1a0dceabe57ef53409a
thirug010 <30732819+thirug010@users.noreply.github.com>
2017-08-26 08:10:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,22 @@ restService.use(bodyParser.json());
 
 restService.get('/linkget', function (req, res) 
 {
-    res.send('Get Response')
+  res.send('Get Response')
 });
 
-restService.post('/echo', function(req, res) {
+restService.get('/link', function(req, res) {
+    /*
+    var userInfo   = JSON.stringify(req.body.originalRequest && req.body.originalRequest.data && req.body.originalRequest.data.user ? req.body.originalRequest.data.user : {});
+    var parameters = JSON.stringify( req.body.result && req.body.result.parameters ? req.body.result.parameters : {} ).replace('device-sub','devicesub');
+    var actionInfo = JSON.stringify(req.body.result && req.body.result.action ? action : {})
+    */
     var deviceName = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceName ? req.body.result.parameters.deviceName : "No such Device in your Home"
     var deviceAction = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceAction ? req.body.result.parameters.deviceAction : "No such Action supported for all devies in your Home"
     
     var speech = deviceName + " is " + deviceAction;
     
-    var url = "http://smarthome2707.ddns.net/wapi/smartLinkDevice?deviceName="+deviceName+"&deviceAction=" + deviceAction    
+    var url = "http://smarthome270
+    7.ddns.net/wapi/smartLinkDevice?userInfo=" + userInfo + '&parameters=' + parameters + '&actionInfo=' + actionInfo
     http.get(url, function(response) {
           var finalData = "";
 
